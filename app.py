a7e87af496ae0e5cdcb3fb48ac9e62754b673e4f
Akhil Agarwala <aagar1@terpmail.umd.edu>
2026-09-10 10:15:04 -0400

    Added first code change to main page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Akhil Agarwala!'
+    return 'Hello World from Akhil Agarwala! I am adding my first code change.'
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
